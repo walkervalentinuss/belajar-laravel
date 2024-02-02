ef5366c2f08e2e500e5d43c32acd3eeac1220be2
walkervalentinuss <walkervalentinuss@gmail.com>
2024-02-03 00:57:13 +0700

    hari ke 3

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     <h1>Halaman About</h1>
-    <h3><?php echo $name; ?></h3> <!-- php echo bisa diganti dengan =  -->
-    <p><?php echo $email; ?></p>
-    <img src="img/<?php echo $image; ?>" alt="<?php echo $name; ?>" width="200">
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
 </body>
 </html>
